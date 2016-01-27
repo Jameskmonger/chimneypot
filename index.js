a57710a8875e36127ce162b072f34e7ef0bc9469
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-01-27 13:15:47 +0000

    All tests passing

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 module.exports = (function() {
   function chimneypot(opts) {
-    if (!opts) {
+    if (!opts || !isOptionsValid(opts)) {
       throw new Error("Required options: port, path, secret");
     }
+  }
+
+  function isOptionsValid(opts) {
+    if (opts.port === undefined || opts.path === undefined) {
+      return false;
+    }
+
+    return true;
   }
 
   chimneypot.prototype = {
