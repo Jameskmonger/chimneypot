82b4802fec056b4f0ce2b6c28282d783ce40e4ee
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-01-27 12:54:48 +0000

    Basic module creation

---
+++ index.js
@@ -0,0 +1,13 @@
+var chimneypot = (function() {
+  function chimneypot() {
+
+  }
+
+  chimneypot.prototype = {
+    constructor: chimneypot
+  };
+
+  return chimneypot;
+});
+
+module.exports = chimneypot;
